e20ecc9fe6db29df150fc3579df676c7da0f53e5
jaddyknight <jaddyknight@gmail.com>
2025-01-01 14:21:23 +0300

    Update and rename script.js to products.js

---
+++ products.js
@@ -0,0 +1,23 @@
+[
+    {
+        "id": 1,
+        "name": "Увлажняющий крем",
+        "description": "Легкий увлажняющий крем для ежедневного использования.",
+        "price": 1200,
+        "images": ["tovar/tovar1_1.jpg", "tovar/tovar1_2.jpg"]
+    },
+    {
+        "id": 2,
+        "name": "Шампунь для волос",
+        "description": "Шампунь с натуральными экстрактами для всех типов волос.",
+        "price": 800,
+        "images": ["tovar/tovar2_1.jpg", "tovar/tovar2_2.jpg"]
+    },
+    {
+        "id": 3,
+        "name": "Маска для лица",
+        "description": "Очищающая маска для лица с эффектом детокса.",
+        "price": 1500,
+        "images": ["tovar/tovar3_1.jpg", "tovar/tovar3_2.jpg"]
+    }
+]
